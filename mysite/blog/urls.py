ce31156b06acc9198c7909847de2a19e09e0bc3e
KingVast <kingvast.wh@gmail.com>
2018-05-26 14:52:16 +0800

    增加博客按月归档

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
         'type/<int:blog_type_pk>',
         views.blogs_with_type,
         name="blogs_with_type"),
+    path(
+        'date/<int:year>/<int:month>',
+        views.blogs_with_date,
+        name="blogs_with_date"),
 ]
