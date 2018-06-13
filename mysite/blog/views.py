9abbd1c5fff4b170a6a172c942a1ab5110ab9010
Kingvast <kingvast.wh@gmail.com>
2018-06-13 14:27:56 +0800

    html表单提交评论

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import get_object_or_404, render
 from django.core.paginator import Paginator
 from django.conf import settings
 from django.db.models import Count
+from django.contrib.contenttypes.models import ContentType
 from .models import Blog, BlogType
 from read_statistics.utils import read_statistics_once_read
+from comment.models import Comment
 
 
 def get_blog_list_common_date(request, blogs_all_list):
@@ -68,6 +70,8 @@ def blogs_with_date(request, year, month):
 def blog_detail(request, blog_pk):
     blog = get_object_or_404(Blog, pk=blog_pk)
     read_cookie_key = read_statistics_once_read(request, blog)
+    blog_content_type = ContentType.objects.get_for_model(blog)
+    comments = Comment.objects.filter(content_type=blog_content_type, object_id=blog.pk)
 
     context = {}
     blog = get_object_or_404(Blog, pk=blog_pk)
@@ -76,6 +80,7 @@ def blog_detail(request, blog_pk):
     context['next_blog'] = Blog.objects.filter(
         created_time__lt=blog.created_time).first()
     context['blog'] = blog
+    context['comments'] = comments
     response = render(request, 'blog/blog_detail.html', context)
     response.set_cookie(read_cookie_key, 'true')
     return response
